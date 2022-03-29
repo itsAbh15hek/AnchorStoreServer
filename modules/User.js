7de9da503db2084b63c7a9ace9c1d2da98f54fad
Abh15hek <itsabh15hek@outlook.com>
2022-03-30 03:51:44 +0530

    removed likes

--- modules/User.js
+++ modules/User.js
@@ -14,7 +14,6 @@ const UserSchema = new mongoose.Schema(
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
     img: { type: String },
-    favourites: { type: Array },
   },
   { timestamps: true }
 );
